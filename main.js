c6b1276a16aa9f7dc047ffdcae70e4a90056bce1
Sidrafatima6601 <165972146+Sidrafatima6601@users.noreply.github.com>
2024-04-13 00:02:57 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var personName = "Sidra Fatima";
+console.log("hello", personName, "would you like to learn some pythone today?");
---
+++ main.ts
@@ -0,0 +1,2 @@
+let personName = "Sidra Fatima";
+console.log("hello", personName, "would you like to learn some pythone today?");
